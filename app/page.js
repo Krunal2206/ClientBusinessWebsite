e6645428d0f7a18c5f54d01e7e12dc4057bcbcb7
Tejashsoni2305 <87148307+Tejashsoni2305@users.noreply.github.com>
2025-01-11 14:17:19 -0800

    dependency changes

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import ContactUsSection from "@/components/ContactUs";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import ServicesSection from "@/components/ServicesSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
